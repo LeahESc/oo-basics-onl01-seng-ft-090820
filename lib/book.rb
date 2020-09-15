2291921aa9d52c7261e4a35b69d57f8324b3a8c0
Leah Schlackman <leahemily1@gmail.com>
2020-09-15 20:09:23 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,29 +8,29 @@ class Book
     @title
   end
   
-  def author= (author)
-    @author = author
-  end
+  # def author= (author)
+  #   @author = author
+  # end
   
-  def author
-    @author
-  end
+  # def author
+  #   @author
+  # end
   
-  def page_count=(pages)
-    @page_count = pages
-  end
+  # def page_count=(pages)
+  #   @page_count = pages
+  # end
   
-  def page_count
-    @page_count
-  end
+  # def page_count
+  #   @page_count
+  # end
   
-  def genre=(genre)
-    @genre = genre
-  end
+  # def genre=(genre)
+  #   @genre = genre
+  # end
   
-  def genre 
-    @genre
-  end
+  # def genre 
+  #   @genre
+  # end
   
   def turn_page 
     puts "Flipping the page...wow, you read fast!"
